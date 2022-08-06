b942721fa7fcef18b31a7e1d38c18c9f39725fe7
Alone-Y154 <yashwanthkris154@gmail.com>
2022-08-06 15:14:47 +0530

    part 2/3 is completed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,15 +2,22 @@ import React from "react";
 import Header from "./Header";
 import Note from "./Note";
 import Footer from "./Footer";
+import data from "../data";
 
 
 
 function App(){
     return(    <div>
         <Header />
-        <Note />
+        {data.map(matter => {
+            return <Note key= {matter.id}
+            title= {matter.title}
+            content= {matter.content} />
+
+        }
+        )        }
         <Footer />
-    </div>)
+    </div>);
 
 }
 
